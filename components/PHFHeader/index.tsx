14f84405cb723696d40b40bd4096ca6136702da7
Pedro Henrique Silva Rosa <pedro.phdois@gmail.com>
2021-05-11 14:20:12 -0300

    #1 animation

--- components/PHFHeader/index.tsx
+++ components/PHFHeader/index.tsx
@@ -13,27 +13,27 @@ export default function PHFHeader() {
         <h2 className={styles.subtitle}>Front-end Developer</h2>
         <div className={styles.social}>
           <Link href="https://www.facebook.com/le.pedro.derp">
-            <a target="_blank">
+            <a target="_blank" className={styles.facebook}>
               <i className="fab fa-facebook-f" />
             </a>
           </Link>
           <Link href="https://wa.me/554891691208">
-            <a target="_blank">
+            <a target="_blank" className={styles.whatsapp}>
               <i className="fab fa-whatsapp" />
             </a>
           </Link>
           <Link href="https://www.instagram.com/pedro_atem/">
-            <a target="_blank">
+            <a target="_blank" className={styles.instagram}>
               <i className="fab fa-instagram" />
             </a>
           </Link>
           <Link href="https://www.linkedin.com/in/pedro-henrique-silva-rosa-94aa7a67/">
-            <a target="_blank">
+            <a target="_blank" className={styles.linkedin}>
               <i className="fab fa-linkedin-in" />
             </a>
           </Link>
           <Link href="https://github.com/phfront">
-            <a target="_blank">
+            <a target="_blank" className={styles.github}>
               <i className="fab fa-github" />
             </a>
           </Link>
